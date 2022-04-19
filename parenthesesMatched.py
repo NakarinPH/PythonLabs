76b53e726c476c4b7d57c30caec686b8dfd9b04d
NakarinPH <nakarin.philangam@gmail.com>
2022-04-19 14:35:28 -0400

    alternative def parenthesesMatched1

--- parenthesesMatched.py
+++ parenthesesMatched.py
@@ -1,4 +1,12 @@
+"""Parentheses are used in mathematical expressions to denote modifications to normal order of operations.
+They must be used in pairs and can be nested inside another pair. For example, parentheses in this expression ((5-2)*(3+6))*4 are matched properly,
+while parentheses in this expression ((2*(3+6)+4)*5 are not.
 
+Develop an algorithm for a function parenthesesMatched, which tests whether parentheses in a mathematical expression are matched properly.
+This function has a parameter oper_list, which is a list that holds the operands and operators of a mathematical expression. For example,
+the oper_list for the expression ((5-2)*(3+6))*4 is ['(', '(', '5', '-', '2', ')', '*', '(', '3', '+', '6', ')', ')', '*', '4'].
+This function returns True if the parentheses are matched properly, and False otherwise. [Hint: Consider using a stack]
+"""
 def parenthesesMatched(oper_list):
     # returns True if the parentheses are matched properly, and False otherwise.
     stk = []
