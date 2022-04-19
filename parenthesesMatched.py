0b9bf528e4d374a0012af0d5b0de5fa7cbf4e87f
NakarinPH <nakarin.philangam@gmail.com>
2022-04-19 14:27:20 -0400

    alternative def parenthesesMatched1

--- parenthesesMatched.py
+++ parenthesesMatched.py
@@ -12,6 +12,22 @@ def parenthesesMatched(oper_list):
     else:
         print('Parenthesis NOT matched')
 
+def parenthesesMatched1(oper_list):
+    stk = []
+    for operands in oper_list:
+        if operands == '(':
+            stk.append(operands)
+        elif operands == ')':
+            if len(stk) == 0:
+                print("Not matched")
+            else:
+                stk.pop()
+    if len(stk) == 0:
+        print("Matched")
+    else:
+        print("Not matched")
+
 
 oper_list = ['(', '(', '5', '-', '2', ')', '*', '(', '3', '+', '6', ')', ')', '*', '4']
 parenthesesMatched(oper_list)
+parenthesesMatched1(oper_list)
