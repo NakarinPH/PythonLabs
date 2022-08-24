d6c9f12430ae087ab353fdf0b516ae3b309e5eb2
NakarinPH <nakarin.philangam@gmail.com>
2022-08-23 20:13:18 -0400

    edited output

--- CSC256/MyGradeExercise.py
+++ CSC256/MyGradeExercise.py
@@ -2,12 +2,12 @@
 # Class: CSC256.0001
 
 
-def calculateMyGrade(labs_score, midterm_score, final_score):
-    labs_weight = 34
-    midterm_weight = 33
-    final_weight = 33
+def calculateMyGrade(assignments_score, midterm_score, final_score):
+    assignments_weight = 30
+    midterm_weight = 35
+    final_weight = 35
 
-    lab_average = (labs_score * labs_weight) / 100
+    lab_average = (assignments_score * assignments_weight) / 100
     midterm_average = (midterm_score * midterm_weight) / 100
     final_average = (final_score * final_weight) / 100
 
@@ -18,9 +18,9 @@ def calculateMyGrade(labs_score, midterm_score, final_score):
 
 def main():
     print("Welcome to My Grade Calculator")
-    labs_score = float(input("Enter your labs score: "))
-    midterm_score = float(input("Enter your midterm score: "))
-    final_score = float(input("Enter your final score: "))
+    labs_score = float(input("Enter your Assignments score: "))
+    midterm_score = float(input("Enter your Midterm Exam score: "))
+    final_score = float(input("Enter your Final Exam score: "))
     final_grade = calculateMyGrade(labs_score, midterm_score, final_score)
     print(" ")
     print("Your average grade is: ", final_grade)
