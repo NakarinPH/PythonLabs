2cf0f8c10f4c2fdc3ff58c03dc159132eebbe591
NakarinPH <nakarin.philangam@gmail.com>
2022-08-23 20:06:45 -0400

    retirement grade

---
+++ CSC256/FullRetirementAge.py
@@ -0,0 +1,13 @@
+# Author: Nakarin Philangam
+# Class: CSC256.0001
+
+def main():
+    print("Social Security Full Retirement Age Calculator")
+
+    birth_year = int(input("Enter the year of birth or to exit: "))
+    while birth_year:
+        birth_month = int(input("Enter the month of birth: "))
+        full_retirement_age = calulate_retirement_age(birth_year,birth_month)
+
+def calulate_retirement_age(birth_year,brith_month):
+    # if birth_year <= 1960
