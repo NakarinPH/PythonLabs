1c011abf317e984c52935b37f378535d4615314e
NakarinPH <nakarin.philangam@gmail.com>
2022-08-27 14:38:23 -0400

    update the validation rule

--- CSC256/MyGradeExercise.py
+++ CSC256/MyGradeExercise.py
@@ -7,9 +7,9 @@ def calculateMyGrade(assignments_score, midterm_score, final_score):
     midterm_weight = 35
     final_weight = 35
 
-    lab_average = (assignments_score * assignments_weight) / 100
-    midterm_average = (midterm_score * midterm_weight) / 100
-    final_average = (final_score * final_weight) / 100
+    lab_average = (float(assignments_score) * assignments_weight) / 100
+    midterm_average = (float(midterm_score) * midterm_weight) / 100
+    final_average = (float(final_score) * final_weight) / 100
 
     average_grade = "{:.2f}".format(lab_average + midterm_average + final_average)
 
@@ -18,9 +18,30 @@ def calculateMyGrade(assignments_score, midterm_score, final_score):
 
 def main():
     print("Welcome to My Grade Calculator")
-    labs_score = float(input("Enter your Assignments score: "))
-    midterm_score = float(input("Enter your Midterm Exam score: "))
-    final_score = float(input("Enter your Final Exam score: "))
+    while True:
+        labs_score = input("Enter your Assignments score: ")
+        if not labs_score.isnumeric():
+            print("Invalid. Please enter a number >= 0")
+            continue
+        else:
+            break
+
+    while True:
+        midterm_score = input("Enter your Midterm Exam score: ")
+        if not midterm_score.isnumeric():
+            print("Invalid. Please enter a number >= 0")
+            continue
+        else:
+            break
+
+    while True:
+        final_score = input("Enter your Final Exam score: ")
+        if not final_score.isnumeric():
+            print("Invalid. Please enter a number >= 0")
+            continue
+        else:
+            break
+
     final_grade = calculateMyGrade(labs_score, midterm_score, final_score)
     print(" ")
     print("Your average grade is: ", final_grade)
